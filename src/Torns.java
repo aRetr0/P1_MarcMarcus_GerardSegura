92eaf2121ccb28331506f4b8e0cd7252bd4bf664
retr0 <gseguraa@edu.tecnocampus.cat>
2024-09-27 12:43:10 +0200

    creada estructura Torns

--- src/Torns.java
+++ src/Torns.java
@@ -1,2 +1,30 @@
-public class Torns {
+import java.util.ArrayList;
+
+public class Torns<E> {
+    private ArrayList<E> llistatTorns;
+
+    public Torns() {
+        // TODO
+    }
+
+    public Torns(String nomFitxer) {
+        // TODO
+    }
+
+    public void afegirTorn(E torn) {
+        // TODO
+    }
+
+    public E agafarPrimerTorn() {
+        // TODO
+        return null;
+    }
+
+    public void guardarAFitxer(String nomFitxer) {
+        // TODO
+    }
+
+    private void carregarDesDeFitxer(String nomFitxer) {
+        // TODO
+    }
 }
