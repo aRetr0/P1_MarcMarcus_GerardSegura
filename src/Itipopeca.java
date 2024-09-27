5793c4f943f3268cc21e4b9ff280e91265a4482b
retr0 <gseguraa@edu.tecnocampus.cat>
2024-09-27 12:45:17 +0200

    creada interfície tipopeca

--- src/Itipopeca.java
+++ src/Itipopeca.java
@@ -1,2 +1,11 @@
 public interface Itipopeca {
+    // NO MODIFICAR
+    // mètodes abstractes: aqui detallem les capçaleres,
+    // les classes que implementen la intefície, hauran d'implementar aquests mètodes
+    public abstract char getTipus();
+    public abstract int getFila();
+    public abstract int getColumna();
+    public abstract void setPosicio(int fila, int columna) throws RuntimeException;
+    // si la posició no és correcta, cal llançar una excepció
+    public abstract boolean fiJoc();
 }
