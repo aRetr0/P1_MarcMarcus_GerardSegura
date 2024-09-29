4f59ee3032b1b8e8340882a860e27db02857d296
mmarcusm <mmarcusm@edu.tecnocampus.cat>
2024-09-29 13:32:06 +0200

    Afegits els metodes que s'han de fer a la classe jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,2 +1,27 @@
-public class Jugador {
+import java.util.ArrayList;
+
+public class Jugador<E> {
+
+    private ArrayList<E> piezasVivas;
+
+    public Jugador(ArrayList<E> piezasVivas) {
+        //TODO
+    }
+
+    public ArrayList<E> getPiezasVivas(){
+        //TODO
+    }
+
+    public void moverPieza(int columnaAnterior, int filaAnterior, int columnaNova, int filaNova){
+        //TODO
+    }
+
+    private E buscarEnPosicion (int filaNova, int columnaNova){
+        //TODO
+    }
+
+    public boolean eliminarPiezaEnPosicion(int columna, int fila){
+        //TODO
+    }
+
 }
